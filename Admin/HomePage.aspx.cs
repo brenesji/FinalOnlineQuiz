6f9d20906b48fe934dd8dbf11c55c58cc0efedee
guillermo0891 <37131729+guillermo0891@users.noreply.github.com>
2018-04-02 22:04:00 -0600

    Add files via upload

--- Admin/HomePage.aspx.cs
+++ Admin/HomePage.aspx.cs
@@ -18,27 +18,42 @@ using System.Web.UI.WebControls;
     {
    
         string connection ="server=psychotestdb.database.windows.net;database=quizdb;user=brenesji;pwd=DesarrolloApp123";
-        string insertSQL = "SELECT * FROM usuarios where ID = '" + Txt_Usuario.Text + "' and contrasena='" + Txt_password.Text + "'";
+        string insertSQL = "SELECT * FROM usuarios where ID = '" + Txt_Usuario.Text.Trim() + "' and contrasena='" + Txt_password.Text.Trim() + "'";
+        string tipoSQL = "SELECT tipo FROM usuarios where ID = '" + Txt_Usuario.Text.Trim() + "' and contrasena='" + Txt_password.Text.Trim() + "'";
+
 
         try
         {
             SqlConnection con = new SqlConnection(connection);
             con.Open();
             SqlCommand cmd = new SqlCommand(insertSQL, con);
+            SqlCommand cmd1 = new SqlCommand(tipoSQL, con);
             string output = cmd.ExecuteScalar().ToString();
+            string tipo = cmd1.ExecuteScalar().ToString();
 
-            if (output == Txt_Usuario.Text)
+            if (output == Txt_Usuario.Text.Trim() & tipo == "U" | tipo == "P")
             {
                 //Aqui creo una variable session
                 Session["user"] = Txt_Usuario.Text;
-                con.Close();
-                con.Dispose();
                 Response.Redirect("~/Admin/Student_Profile.aspx");
             }
+            else if (output == Txt_Usuario.Text.Trim() & tipo == "A" )
+            {
+                //Aqui creo una variable session
+                Session["user"] = Txt_Usuario.Text;
+                Response.Redirect("~/Admin/Menu.aspx");
+            }
             else
+            {
                 Lbl_Error.Visible = true;
-            Lbl_Error.Text = "Error: Nombre de Usuario o Contrasena incorrecta";
+                Lbl_Error.Text = "Error: Nombre de Usuario o Contrasena incorrecta";
+            }
+
+            con.Close();
+            con.Dispose();
+
         }
+
         catch (Exception)
         {
             Lbl_Error.Visible = true;
--- Admin/New_User.aspx.cs
+++ Admin/New_User.aspx.cs
@@ -65,30 +65,27 @@ public partial class Admin_New_User : System.Web.UI.Page
 
     protected void DropDownList_Provincia_SelectedIndexChanged(object sender, EventArgs e)
     {
-        string selectSQL = "SELECT nombre from cantones where ID_Provincia = ";
-        for (int i = 1; i < 7; i++)
-        {
-            if (DropDownList_Provincia.SelectedItem.Value.Equals(i.ToString()))
-            {
-                selectSQL = selectSQL + i.ToString() + ";"; Add_Canton(selectSQL);
-            }
-        }
+
+        DropDownList_Canton.Items.Clear();
+        Add_Canton(Convert.ToInt32(DropDownList_Provincia.SelectedItem.Value));
+        
     }
 
-    public void Add_Canton(string selectSQL)
+    public void Add_Canton(int idprovincia)
     {
+
         try
         {
             string connection = "server=psychotestdb.database.windows.net;database=quizdb;user=brenesji;pwd=DesarrolloApp123";
-            int CONT = 0;
 
             SqlConnection con = new SqlConnection(connection);
-            SqlCommand cmd = new SqlCommand(selectSQL, con);
+            SqlCommand cmd = new SqlCommand("SELECT ID_Canton, nombre from cantones where ID_Provincia=@ID_Provincia", con);
+            cmd.Parameters.AddWithValue("ID_Provincia", idprovincia);
             SqlDataReader reader;
 
             ListItem newItem = new ListItem();
             newItem.Text = "Canton";
-            newItem.Value = CONT.ToString();
+            newItem.Value = "0";
             DropDownList_Canton.Items.Add(newItem);
             con.Open();
             reader = cmd.ExecuteReader();
@@ -97,7 +94,7 @@ public partial class Admin_New_User : System.Web.UI.Page
             {
                 ListItem newItem1 = new ListItem();
                 newItem1.Text = reader["nombre"].ToString();
-                newItem1.Value = (CONT + 1).ToString();
+                newItem1.Value = reader["ID_Canton"].ToString();
                 DropDownList_Canton.Items.Add(newItem1);
             }
             reader.Close();
@@ -114,33 +111,26 @@ public partial class Admin_New_User : System.Web.UI.Page
 
     protected void DropDownList_Canton_SelectedIndexChanged1(object sender, EventArgs e)
     {
+        DropDownList_Distrito.Items.Clear();
+        Add_Distrito(Convert.ToInt32(DropDownList_Canton.SelectedItem.Value));
 
-        //DropDownList_Canton.SelectedIndex = 0; //Reset Dropdownlist
-        string selectSQL = "SELECT nombre FROM Distritos Where ID_Canton =";
-        for (int i = 1; i < 82; i++)
-        {
-            if (DropDownList_Canton.SelectedItem.Value.Equals(i.ToString()))
-            {
-                selectSQL = selectSQL + i.ToString() + ";"; Add_Distrito(selectSQL);
-            }
-        }
     }
 
 
-    public void Add_Distrito(string selectSQL)
+    public void Add_Distrito(int idcanton)
     {
         try
         {
             string connection = "server=psychotestdb.database.windows.net;database=quizdb;user=brenesji;pwd=DesarrolloApp123";
-            int CONT = 0;
 
             SqlConnection con = new SqlConnection(connection);
-            SqlCommand cmd = new SqlCommand(selectSQL, con);
+            SqlCommand cmd = new SqlCommand("SELECT ID_Distrito, nombre FROM Distritos Where ID_Canton=@ID_Canton", con);
+            cmd.Parameters.AddWithValue("ID_Canton", idcanton);
             SqlDataReader reader;
 
             ListItem newItem = new ListItem();
             newItem.Text = "Distrito";
-            newItem.Value = CONT.ToString();
+            newItem.Value = "0";
             DropDownList_Distrito.Items.Add(newItem);
             con.Open();
             reader = cmd.ExecuteReader();
@@ -149,7 +139,7 @@ public partial class Admin_New_User : System.Web.UI.Page
             {
                 ListItem newItem1 = new ListItem();
                 newItem1.Text = reader["nombre"].ToString();
-                newItem1.Value = (CONT + 1).ToString();
+                newItem1.Value = reader["ID_Distrito"].ToString();
                 DropDownList_Distrito.Items.Add(newItem1);
             }
             reader.Close();
@@ -244,8 +234,6 @@ public partial class Admin_New_User : System.Web.UI.Page
     protected void Submit_Click(object sender, EventArgs e)
     {
 
-//        DateTime dt = DateTime.Now;
-       // DateTime.TryParseExact(Txt_Date.Text, "yy-MM-dd", System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None, out dt);
 
         Insert_Student(txt_IDusuario.Text, txt_Name.Text, txt_FirstName.Text, txt_LastName.Text,
         DropDownList_Provincia.Text, DropDownList_Canton.Text, DropDownList_Distrito.Text, DropDownList_HighSchool.Text,
@@ -268,7 +256,7 @@ public partial class Admin_New_User : System.Web.UI.Page
     {
         try
         {
-            string insertSQL = "INSERT INTO usuarios VALUES ('" + iID_Usuario + "','" + iNombre + "','" + iPrimer_Apellido + "','" + iSegundo_Apellido + "','" + iProvincia + "','" + iCanton + "','" + iDistrito + "','" + iInstitucion + "','" + isexo + "','" + itelefono + "','" + iFecha + "','" + iNacionalidad + "','" + iPassword1 + "','" + iCedula + "','1' );";
+            string insertSQL = "INSERT INTO usuarios VALUES ('" + iID_Usuario + "','" + iNombre + "','" + iPrimer_Apellido + "','" + iSegundo_Apellido + "','" + iProvincia + "','" + iCanton + "','" + iDistrito + "','" + iInstitucion + "','" + isexo + "','" + itelefono + "','" + iFecha + "','" + iNacionalidad + "','" + iPassword1 + "','" + iCedula + "','U' );";
             string connection = "server=psychotestdb.database.windows.net;database=quizdb;user=brenesji;pwd=DesarrolloApp123";
             SqlConnection con = new SqlConnection(connection);
             SqlCommand cmd = new SqlCommand(insertSQL, con);
